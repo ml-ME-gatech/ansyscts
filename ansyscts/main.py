c803898d32de7e3c84c58b28c8f9001a583f33ae
ml-ME-gatech <61564229+ml-ME-gatech@users.noreply.github.com>
2025-03-09 14:58:31 -0400

    Bad Account Specification
    in pace

--- ansyscts/main.py
+++ ansyscts/main.py
@@ -71,7 +71,7 @@ def main():
     parser.add_argument('--debug',action = 'store_true',help="Enable debug mode.")
     parser.add_argument('--max_workers',type = int,default = 5,help = 'Maximum number of workers for the thread pool')
     parser.add_argument('--queue',type = str,default = 'inferno',help = 'Queue to submit jobs to')
-    parser.add_argument('--account',type = str,default = 'gt-my14-paid',help = 'Account to charge')
+    parser.add_argument('--account',type = str,default = 'gts-my14-paid',help = 'Account to charge')
     
     args = parser.parse_args()
     assert args.smode in {'running','interrupted'}, 'mode must be either running or interrupted'
